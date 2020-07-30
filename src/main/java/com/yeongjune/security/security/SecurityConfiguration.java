1a253ef3750ce7624d9722c01f54a5caa4a5b312
hongyeongjune <hongyeongjune@naver.com>
2020-07-30 16:36:25 +0900

    Authority Based Authorization

--- src/main/java/com/yeongjune/security/security/SecurityConfiguration.java
+++ src/main/java/com/yeongjune/security/security/SecurityConfiguration.java
@@ -21,11 +21,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //                .and()
 //                .withUser("hongguri").password(passwordEncoder().encode("hongguri")).roles("USER")
 //                .and()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+                .withUser("user")
+                .password(passwordEncoder().encode("user123"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager")).roles("MANAGER");
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER")
+                .authorities("ACCESS_TEST1");
     }
 
     @Override
@@ -37,6 +45,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
     }
